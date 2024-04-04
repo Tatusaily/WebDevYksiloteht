d2bf043fd3c445beb6153b1e4a4440c588e596e0
Tatu <tatu.sailynoja@gmail.com>
2024-04-04 14:20:16 +0300

    fix map by fixing typo, tweak styling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const coord = [60.223671, 25.078039, 19]
-const map = L.map('mapbox').setView(coord);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+const coord = [60.223671, 25.078039]
+const zoom = 13
+const map = L.map('mapbox').setView(coord, zoom);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }).addTo(map);
