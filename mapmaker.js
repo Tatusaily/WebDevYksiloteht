c8a99bbec051a25489066f74a596d5f2bc9cc1a6
Tatu <tatu.sailynoja@gmail.com>
2024-04-05 16:19:34 +0300

    improve restaurant data gathering correctly order restaurants and push distance value

--- mapmaker.js
+++ mapmaker.js
@@ -1,7 +1,8 @@
 const coord = [60.223671, 25.078039]
 const zoom = 13
-const map = L.map('mapbox').setView(coord, zoom);
+const leafmap = L.map('mapbox').setView(coord, zoom);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-}).addTo(map);
+}).addTo(leafmap);
+const markergroup = L.layerGroup().addTo(leafmap);
--- restaurantscipt.js
+++ restaurantscipt.js
@@ -1,19 +1,22 @@
 const restaurants = [];
-// Get restaurants to array
+const usercoords = [];
+
 (async function(){
     try{
-        console.log("Fetching restaurants...");
-        await fetch('https://10.120.32.94/restaurant/api/v1/restaurants', {signal: AbortSignal.timeout(2000)})
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(restaurant => {
-              restaurants.push(restaurant);
-            });
-            console.log("Restaurants added to array");
+        Promise.all([getUserPos(),getRestaurants()]).then(() => {
+        console.log("Promises resolved");
+        // Add distance to restaurants and sort
+        restaurants.forEach((resta) => {
+            resta.distance = leafmap.distance([usercoords[0], usercoords[1]], [resta.location.coordinates[1], resta.location.coordinates[0]]);
+        })
+        restaurants.sort((a, b) => a.distance - b.distance);
+        console.log("Restaurants sorted");
+        refreshRestRestaurants();
         });
+
     }catch(error){
         console.error(error);
-        window.alert("Error fetching restaurants");
+        window.alert("Encoutered an error. :(");
     }
 })();
 
@@ -25,3 +28,84 @@ const elementMaker = (type, text = "", id) => {
     element.id = id;
     return element;
   };
+
+/**
+ * Refresh restaurants on map and on the list
+ */
+const refreshRestRestaurants = () => {
+    markergroup.clearLayers();
+    const list = elementMaker("ul", "", "restList");
+    restaurants.forEach(restaurant => {
+
+        const marker = L.marker([restaurant.location.coordinates[1], restaurant.location.coordinates[0]]).addTo(markergroup);
+        marker.on('click', function(){
+            //TODO: Show menu to user
+            console.log(restaurant);
+        });
+
+        const listItem = elementMaker("li", restaurant.name);
+        listItem.onclick = function(){
+            leafmap.setView([restaurant.location.coordinates[1], restaurant.location.coordinates[0]], 13);
+        };
+        listItem.appendChild(elementMaker("ul", restaurant.distance.toFixed(1) + " km"));
+    });
+};
+
+/**
+ * Get restaurants from API
+*/
+async function getRestaurants(){
+    try{
+        console.log("Fetching restaurants...");
+        return fetch('https://10.120.32.94/restaurant/api/v1/restaurants', {signal: AbortSignal.timeout(2000)})
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data); // Check the data
+            // Add restaurants to array
+            data.forEach(restaurant => {
+                restaurants.push(restaurant);
+            });
+            // Add markers to map
+            restaurants.forEach(restaurant => {
+                const marker = L.marker([restaurant.location.coordinates[1], restaurant.location.coordinates[0]]).addTo(markergroup);
+                marker.on('click', function(){
+                    //TODO: Show menu to user
+                    console.log(restaurant);
+                });
+            });
+        });
+        return Promise.resolve("Restaurants fetched");
+    }catch(error){
+        console.error(error);
+        window.alert("Error fetching restaurants");
+    }
+}
+
+// USERPOS SCRIPTS
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+function success(pos) {
+    usercoords.push(pos.coords.latitude, pos.coords.longitude);
+    console.log(usercoords);
+}
+async function getUserPos(){
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 10000, // 10 seconds
+        maximumAge: 0,
+      };
+    try{
+        console.log("Fetching user position...");
+        navigator.geolocation.getCurrentPosition(success, error, options);
+        Promise.resolve("User position fetched");
+    }catch(error){
+        console.error(error);
+        window.alert("Error fetching user position");
+    }
+};
